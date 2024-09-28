0a5943c0dbcace458f3f6c5fa9af3122473b690b
adanshah13579 <106953575+adanshah13579@users.noreply.github.com>
2024-09-29 00:58:34 +0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,20 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // Import HashRouter instead of BrowserRouter
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";  // Import PersistGate
-import { store, persistor } from "./store/store.js";  // Ensure persistor is imported as well
+import { PersistGate } from "redux-persist/integration/react"; // Import PersistGate
+import { store, persistor } from "./store/store.js"; // Ensure persistor is imported as well
 import { Toaster } from "./components/ui/toaster.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     {/* PersistGate delays rendering until the persisted state is rehydrated */}
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
         <Toaster />
-      </BrowserRouter>
+      </HashRouter>
     </PersistGate>
   </Provider>
 );
