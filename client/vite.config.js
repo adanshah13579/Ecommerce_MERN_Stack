463ddc6b884e88479f2a73be5a9def953a065d18
adanshah13579 <106953575+adanshah13579@users.noreply.github.com>
2024-09-29 02:54:38 +0500

    v5

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',  // Ensures that paths to assets are relative
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  build: {
+    outDir: 'dist' // Ensure the output directory is set to 'dist'
+  }
 })
-
