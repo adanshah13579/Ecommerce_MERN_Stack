ebbdd02cc6fc880b6d4f4dc0704d7cc3e9c88f31
adanshah13579 <106953575+adanshah13579@users.noreply.github.com>
2024-09-29 00:33:00 +0500

    v5

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,18 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store/store.js";
+import { PersistGate } from "redux-persist/integration/react";  // Import PersistGate
+import { store, persistor } from "./store/store.js";  // Ensure persistor is imported as well
 import { Toaster } from "./components/ui/toaster.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-      <Toaster />
-    </Provider>
-  </BrowserRouter>
+  <Provider store={store}>
+    {/* PersistGate delays rendering until the persisted state is rehydrated */}
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // localStorage by default
 import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin/products-slice";
 import adminOrderSlice from "./admin/order-slice";
-
 import shopProductsSlice from "./shop/products-slice";
 import shopCartSlice from "./shop/cart-slice";
 import shopAddressSlice from "./shop/address-slice";
@@ -11,22 +12,40 @@ import shopSearchSlice from "./shop/search-slice";
 import shopReviewSlice from "./shop/review-slice";
 import commonFeatureSlice from "./common-slice";
 
+// Redux persist configuration for specific reducers
+const persistConfig = {
+  key: 'root',   // root key for local storage
+  storage,       // storage method (localStorage in this case)
+  whitelist: ['auth', 'shopCart'],  // The slices you want to persist
+};
+
+// Combine your reducers
+const rootReducer = {
+  auth: authReducer,
+  adminProducts: adminProductsSlice,
+  adminOrder: adminOrderSlice,
+  shopProducts: shopProductsSlice,
+  shopCart: shopCartSlice,
+  shopAddress: shopAddressSlice,
+  shopOrder: shopOrderSlice,
+  shopSearch: shopSearchSlice,
+  shopReview: shopReviewSlice,
+  commonFeature: commonFeatureSlice,
+};
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, combineReducers(rootReducer));
+
+// Configure store with persisted reducer
 const store = configureStore({
-  reducer: {
-    auth: authReducer,
-
-    adminProducts: adminProductsSlice,
-    adminOrder: adminOrderSlice,
-
-    shopProducts: shopProductsSlice,
-    shopCart: shopCartSlice,
-    shopAddress: shopAddressSlice,
-    shopOrder: shopOrderSlice,
-    shopSearch: shopSearchSlice,
-    shopReview: shopReviewSlice,
-
-    commonFeature: commonFeatureSlice,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,  // Disable serialization checks for redux-persist
+    }),
 });
 
-export default store;
+// Create a persistor
+const persistor = persistStore(store);
+
+export { store, persistor };
